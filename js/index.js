04fb61b66fdea4507d2117795998f316c4ed94f4
yajamon <yajamon.tatsuki@gmail.com>
2017-12-18 20:18:42 +0900

    Can toggle state of cell

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ class Cell {
     constructor(){
         this.isAlive = false;
     }
+    toggleState(){
+        this.isAlive = !this.isAlive;
+    }
 }
 class Field {
     /**
@@ -25,6 +28,23 @@ class Field {
         }
         this.data = rows;
     }
+    /**
+     *
+     * @param {number} x
+     * @param {number} y
+     */
+    cellByIndex(x, y){
+        return this.data[y][x];
+    }
+    /**
+     *
+     * @param {number} x
+     * @param {number} y
+     */
+    toggleState(x, y){
+        const cell = this.cellByIndex(x, y);
+        cell.toggleState();
+    }
 }
 class Renderer {
     /**
@@ -37,15 +57,36 @@ class Renderer {
      * @param {Field} field
      */
     setup(field) {
-        const data = field.data;
+        this.field = field;
+        const data = this.field.data;
         for (let rowIndex = 0; rowIndex < data.length; rowIndex++) {
             const cols = data[rowIndex];
             const row = document.createElement("tr");
             for (let colIndex = 0; colIndex < cols.length; colIndex++) {
                 const col = document.createElement("td");
+                col.dataset["x"] = colIndex;
+                col.dataset["y"] = rowIndex;
                 row.appendChild(col);
             }
             this.root.appendChild(row);
+        }
+    }
+
+    /**
+     * @param {number} x
+     * @param {number} y
+     */
+    renderCell(x, y){
+        const cell = this.field.cellByIndex(x, y);
+        const dom = this.root
+            .children.item(y)
+            .children.item(x);
+        if (cell.isAlive) {
+            dom.classList.add("alive");
+            dom.classList.remove("dead");
+        } else {
+            dom.classList.remove("alive");
+            dom.classList.add("dead");
         }
     }
 }
@@ -70,11 +111,31 @@ class App {
         this.field.reset();
         this.renderer.setup(this.field);
     }
+    /**
+     *
+     * @param {number} x
+     * @param {number} y
+     */
+    toggleState(x, y){
+        // console.log(x, y);
+        this.field.toggleState(x, y);
+        this.renderer.renderCell(x, y);
+    }
 }
 
 const app = new App(15, 10);
 window.addEventListener("DOMContentLoaded", ()=>{
-    const renderer = new Renderer(document.getElementById("root"));
+    const root = document.getElementById("root");
+    const renderer = new Renderer(root);
     app.setRenderer(renderer);
     app.initialize();
+
+    root.addEventListener("click", (event)=>{
+        Array.from(root.querySelectorAll("td"))
+            .filter(dom => dom === event.target)
+            .forEach(element => {
+                let data = element.dataset;
+                app.toggleState(data["x"], data["y"]);
+            });
+    });
 });
